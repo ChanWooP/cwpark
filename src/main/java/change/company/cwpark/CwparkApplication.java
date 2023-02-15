b89ff8343a4cb54e10d25c0b5b89a0deb689fc26
cksdn4002 <cksdn4002@gmail.com>
2023-02-15 10:31:11 +0900

    Initial commit

---
+++ src/main/java/change/company/cwpark/CwparkApplication.java
@@ -0,0 +1,13 @@
+package change.company.cwpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/change/company/cwpark/CwparkApplicationTests.java
@@ -0,0 +1,13 @@
+package change.company.cwpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
