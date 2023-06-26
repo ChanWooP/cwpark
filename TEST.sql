001761a61f5f9c5d31134aeed1c603e9ef0bd1f6
ChanWooP <cksdn4002@gmail.com>
2023-06-26 13:54:20 +0900

    1. 전체매출현황 프론트엔드 완료

--- TEST.sql
+++ TEST.sql
@@ -1,39 +1,28 @@
-INSERT INTO menu(parent_num, depth, NAME, path) 
-VALUES (NULL, 0, '메뉴', null);
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (1, 1, '대1');
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (2, 2, '중1');
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (1, 1, '대2');
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (4, 2, '중2');
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (2, 2, '중3');
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (2, 2, '중4');
-INSERT INTO menu(parent_num, depth, NAME) 
-VALUES (4, 2, '중5');
+INSERT INTO Sale(STORE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, '20230626', 10000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개
+INSERT INTO Sale(STORE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, '20230626', 10500, 3, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개 크림 1개
+INSERT INTO Sale(STORE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, '20230626', 12000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 딸기 2개
+INSERT INTO Sale(STORE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, '20230626', 10000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개
+INSERT INTO Sale(STORE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, '20230626', 13500, 3, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개 커피 1개
 
-COMMIT;
+INSERT INTO SaleItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 37, 4, '20230626', 10000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개
+INSERT INTO SaleItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 37, 5, '20230626', 10000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개
+INSERT INTO SaleItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 38, 6, '20230626', 12000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 딸기 2개
+INSERT INTO SaleItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 37, 7, '20230626', 10000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개
+INSERT INTO SaleItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 37, 8, '20230626', 10000, 2, SYSDATE(), NULL, SYSDATE(), NULL); -- 누텔라 2개
+INSERT INTO SaleItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 40, 8, '20230626', 3500, 1, SYSDATE(), NULL, SYSDATE(), NULL); -- 커피 1개
 
-/* 공백은 스페이스바 아니고 문자사용 'ㅤ' */
-SELECT A.id
-	  , A.parent_num
-	  , A.depth
-	  , case A.depth when 1 then CONCAT('ㅤ└ ', A.name)
-	         		  when 2 then CONCAT('ㅤㅤ└ ', A.name)
-	         		  ELSE A.name END AS menu_name
-	  , case A.depth when 1 then A.id
-	  					  when 2 then A.parent_num
-	  					  ELSE A.id END AS order_id
-	  , A.path
-	  , A.name
-	  , (SELECT COUNT(1) FROM menu WHERE parent_num = A.id) AS childCnt
-  FROM menu A 
-  LEFT OUTER JOIN menu B 
-    ON A.parent_num = B.id
-ORDER BY order_id, A.id
-;
+INSERT INTO SalePlusItem(STORE_ID, ITEM_ID, SALE_ID, SALE_DATE, AMT, QTY, REG_DATE, REG_ID, UPD_DATE, UPD_ID) 
+VALUES(8, 17, 5, '20230626', 500, 1, SYSDATE(), NULL, SYSDATE(), NULL); -- 크림 1개
 
-SELECT * FROM store;
+SELECT * FROM SalePlusItem
