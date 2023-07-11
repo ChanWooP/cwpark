7895c9810c0580c18c8829e9f66e1ea4b75e4fe5
ChanWooP <cksdn4002@gmail.com>
2023-07-11 21:16:21 +0900

    1. redis 설정 추가

--- src/main/java/change/company/cwpark/CwparkApplication.java
+++ src/main/java/change/company/cwpark/CwparkApplication.java
@@ -3,11 +3,13 @@ package change.company.cwpark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"change.company.cwpark.data.repository"})
 public class CwparkApplication {
 
 	public static void main(String[] args) {
